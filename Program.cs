6c5c25e9bfb5f1a1f6860b5a1ff030c25c842b61
harpreets595 <47327250+harpreets595@users.noreply.github.com>
2020-06-28 17:11:48 -0400

    feat: basic http request done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace httpRequest_cSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        HttpClient client = new HttpClient();
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello harpreet!");
-            
+            Program program = new Program();
+
+            await program.GetTodoItems();
+
         }
+
+        private async Task GetTodoItems()
+        {
+            string responce = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+
+
+            Console.WriteLine(responce);
+        }
+
     }
 }
